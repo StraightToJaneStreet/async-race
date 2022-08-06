8a86265f03ec496dadf0b109ce288b57c383ae42
Straight To <straighttojanestreet@rs.school>
2022-08-06 16:43:14 +0300

    feat: update Garage page

--- async-race/src/view/Pages/Garage.tsx
+++ async-race/src/view/Pages/Garage.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 
 import CarConfiguration from '../components/CarConfiguration';
 import Button from '../components/Button';
+import { RootState } from '../../model/store';
+import { Car } from '../../core/Car';
+import { selectCars } from '../../model/feature/garage/slice';
+import { connect } from 'react-redux';
+import CarsList from '../components/CarsList';
+import Context from '../AppContext';
 
 interface CreateCarProps {
-  handleCreate: () => void
+  handleCreate: (name: string, color: string) => void
 }
 
 function CreateCar({ handleCreate }: CreateCarProps) {
+  const [color, setColor] = useState('#000000');
+  const [name, setName] = useState('');
+
   return (
     <div>
-      <CarConfiguration/>
-      <Button handleClick={handleCreate} label='Create'/>
+      <CarConfiguration udpateColor={setColor} updateName={setName}/>
+      <Button handleClick={() => handleCreate(name, color)} label='Create'/>
     </div>
   );
 }
@@ -29,26 +38,42 @@ function UpdateCar({ handleUpdate }: UpdateCarProps) {
   );
 }
 
-function Garage() {
-  const handleCreateCar = () => {};
-  const handleUpdateCar = () => {};
-  const handleRaceStart = () => {};
-  const handleGenerateCars = () => {};
-  const handleReset = () => {};
+interface GarageProps {
+  cars: Car[]
+}
+
+const mapState = (state: RootState): GarageProps => {
+  const cars = selectCars(state.garage);
+  return { cars };
+}
+
+function Garage({ cars }: GarageProps) {
+  const context = useContext(Context);
+
+  const {
+    handleCreateCar,
+    handleUpdateCar,
+    handleRaceStart,
+    handleGenerateCars,
+    handleReset
+  } = context;
 
   return (
     <>
       <h1>Garage</h1>
-      <CreateCar handleCreate={handleCreateCar}/>
+      <CreateCar handleCreate={(name: string, color) => handleCreateCar(name, color)}/>
       <UpdateCar handleUpdate={handleUpdateCar}/>
       <div>
         <Button handleClick={handleRaceStart} label='Race'/>
         <Button handleClick={handleReset} label='Reset'/>
         <Button handleClick={handleGenerateCars} label='Generate cars'/>
       </div>
-      <h2>Garage ({})</h2>
+      <h2>Garage ({cars.length})</h2>
+      <div className="garage__cars">
+        <CarsList cars={cars.filter((_car, ind) => ind < 20)}/>
+      </div>
     </>
   );
 }
 
-export default Garage;
+export default connect(mapState)(Garage);
