299b2e790d01ff615abfb0462e01e238a3fc33ca
Straight To <straighttojanestreet@rs.school>
2022-08-06 16:44:43 +0300

    feat: bind controllers methods to AppContext

--- async-race/src/App.ts
+++ async-race/src/App.ts
@@ -1,22 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import PagesController from './controller/PagesController';
+import GarageController from './controller/GarageController';
 
 import AppView from './view/App';
+import { AppContext } from './view/AppContext';
 
 export default class App {
   reactRoot: ReactDOM.Root;
   pagesController =  new PagesController();
-
+  garageController = new GarageController()
   constructor(root: HTMLElement) {
     this.reactRoot = ReactDOM.createRoot(root);
   }
 
   run() {
+    const appContext: AppContext = {
+      handleSetPage: this.pagesController.setPage.bind(this.pagesController),
+      handleCreateCar: this.garageController.createCar.bind(this.garageController),
+      handleGenerateCars: this.garageController.generateCars.bind(this.garageController),
+      handleRaceStart: () => {},
+      handleReset: () => {},
+      handleUpdateCar: () => {},
+    };
+
     this.reactRoot.render(React.createElement(AppView, {
-      context: {
-        setPage: this.pagesController.setPage.bind(this.pagesController)
-      }
+      context: appContext
     }, null));
   }
 }
