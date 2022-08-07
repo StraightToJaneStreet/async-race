c701924abbd35dd1d3e4c963b1ed19d5e6ab5706
Straight To <straighttojanestreet@rs.school>
2022-08-08 01:17:22 +0300

    feat: add RacingService

---
+++ async-race/src/controller/RacingService.ts
@@ -0,0 +1,21 @@
+import ApiService from "./ApiService";
+
+export default class RacingService {
+  protected static instance: RacingService | null;
+
+  startCar(carId: number) {
+    const api = ApiService.getInstance();
+    api.startEngine(carId);
+  }
+
+  startRace() {
+
+  }
+
+  static getInstance(): RacingService {
+    if (RacingService.instance === null) {
+      RacingService.instance = new RacingService();
+    }
+    return RacingService.instance;
+  }
+}
