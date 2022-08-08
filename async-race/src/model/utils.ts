87bc7818e824fcec7a8e2f93c6f95bb3a96673ae
Straight To <straighttojanestreet@rs.school>
2022-08-09 00:23:19 +0300

    feat: add actionCreatorFactory

---
+++ async-race/src/model/utils.ts
@@ -0,0 +1,7 @@
+import { createAction } from "@reduxjs/toolkit";
+
+export const createActionCreatorFactory
+  = <TPrefix extends string>(PREFIX: TPrefix) => <Payload, TSuffix extends string>(suffix: TSuffix) => {
+  const actionType: `${TPrefix}/${TSuffix}` = `${PREFIX}/${suffix}`;
+  return createAction<Payload, `${TPrefix}/${typeof suffix}`>(actionType);
+}
