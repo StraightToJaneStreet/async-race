6dbfe63cf12efb76005f117c3abcf1a8c0d0f4bf
Straight To <straighttojanestreet@rs.school>
2022-08-09 01:12:04 +0300

    feat: add GaragePage with working pagination

---
+++ async-race/src/view/Pages/Garage/GaragePage.tsx
@@ -0,0 +1,75 @@
+import React, { useContext } from 'react';
+import { useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
+import { Car } from '../../../core/Car';
+import { CARS_PER_PAGE } from '../../../core/Contants';
+import { selectCars } from '../../../model/feature/garage/slice';
+
+import { actionDecrementPage, actionIncrementPage } from '../../../model/feature/garagePages';
+import { RootState } from '../../../model/store';
+
+import AppContext from '../../AppContext';
+
+import Button from '../../components/Button';
+import CreateCar from './CreateCar';
+import UpdateCar from './UpdateCar';
+import TrackList from '../../components/TrackList';
+
+interface GaragePageProps {
+  page: number;
+  cars: Car[];
+}
+
+const mapToState = (state: RootState): GaragePageProps => {
+  return {
+    page: state.garagePage.currentPage,
+    cars: selectCars(state.garage)
+  };
+}
+
+function GaragePage({ page, cars }: GaragePageProps) {
+  const context = useContext(AppContext);
+
+  const leftPaginationBorder = page * CARS_PER_PAGE;
+  const rightPaginationBorder = leftPaginationBorder + CARS_PER_PAGE;
+
+  const carsForDisplay = cars.slice(
+    leftPaginationBorder,
+    rightPaginationBorder
+  );
+
+  const {
+    handleRaceStart,
+    handleGenerateCars,
+    handleReset
+  } = context;
+
+  const dispatch = useDispatch();
+
+  return (
+    <div className="garage__page">
+      <CreateCar/>
+      <UpdateCar/>
+      <div className="garage__buttons">
+        <Button handleClick={handleRaceStart} label='Race'/>
+        <Button handleClick={handleReset} label='Reset'/>
+        <Button handleClick={handleGenerateCars} label='Generate cars'/>
+      </div>
+      <h2 className="garage__cars-counter">Garage ({cars.length})</h2>
+      <h3 className="garage__page-number">Page #{page + 1}</h3>
+      <div className="garage__cars">
+        <TrackList cars={carsForDisplay}/>
+      </div>
+      <div className="garage__navigation">
+        <Button
+          label='Prev'
+          enabled={page !== 0}
+          handleClick={() => dispatch(actionDecrementPage())} />
+
+        <Button label='Next' handleClick={() => dispatch(actionIncrementPage())} />
+      </div>
+    </div>
+  )
+}
+
+export default connect(mapToState)(GaragePage);
--- async-race/src/view/Pages/Garage/index.tsx
+++ async-race/src/view/Pages/Garage/index.tsx
@@ -1,49 +1,13 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
-import Button from '../../components/Button';
-import { RootState } from '../../../model/store';
-import { Car } from '../../../core/Car';
-import { selectCars } from '../../../model/feature/garage/slice';
-import { connect } from 'react-redux';
-import TrackList from '../../components/TrackList';
-import Context from '../../AppContext';
+import GaragePage from './GaragePage';
 
-import CreateCar from './CreateCar';
-import UpdateCar from './UpdateCar';
-
-interface GarageProps {
-  cars: Car[]
-}
-
-const mapState = (state: RootState): GarageProps => {
-  const cars = selectCars(state.garage);
-  return { cars };
-}
-
-function Garage({ cars }: GarageProps) {
-  const context = useContext(Context);
-
-  const {
-    handleRaceStart,
-    handleGenerateCars,
-    handleReset
-  } = context;
-
+function Garage() {
   return (
     <div className="garage">
-      <CreateCar/>
-      <UpdateCar/>
-      <div className="garage__buttons">
-        <Button handleClick={handleRaceStart} label='Race'/>
-        <Button handleClick={handleReset} label='Reset'/>
-        <Button handleClick={handleGenerateCars} label='Generate cars'/>
-      </div>
-      <h2 className="garage__cars-counter">Garage ({cars.length})</h2>
-      <div className="garage__cars">
-        <TrackList cars={cars.filter((_car, ind) => ind < 20)}/>
-      </div>
+      <GaragePage/>
     </div>
   );
 }
 
-export default connect(mapState)(Garage);
+export default Garage;
