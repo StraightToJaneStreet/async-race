dd661af8b52eef470d794483e0cf9e670175ffd4
Straight To <straighttojanestreet@rs.school>
2022-08-08 16:38:56 +0300

    feat: add garageController initialization

--- async-race/src/App.ts
+++ async-race/src/App.ts
@@ -19,6 +19,7 @@ export default class App {
   }
 
   run() {
+    this.garageController.initialize();
     const appContext: AppContext = {
       handleSetPage: this.pagesController.setPage.bind(this.pagesController),
       handleCreateCar: this.garageController.createCar.bind(this.garageController),
