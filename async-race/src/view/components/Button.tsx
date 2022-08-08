15943e8335b70df4b1ba828e64365da577a605a6
Straight To <straighttojanestreet@rs.school>
2022-08-09 01:02:05 +0300

    feat: add Enabled optional parameter for Button component

--- async-race/src/view/components/Button.tsx
+++ async-race/src/view/components/Button.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
 interface Props {
-  handleClick: () => void,
-  label: string
+  handleClick: () => void;
+  label: string;
+  enabled?: boolean;
 }
 
-export default function Button({ label, handleClick }: Props) {
+export default function Button({ label, handleClick, enabled }: Props) {
+  const buttonEnabled = enabled === undefined ? true : enabled;
+
   return (
     <button
       className='button'
+      disabled={!buttonEnabled}
       onClick={handleClick}>
         {label}
     </button>
