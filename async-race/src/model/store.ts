b99d9b85553ae84ec9bc67d9fea878bdfe19d78a
Straight To <straighttojanestreet@rs.school>
2022-08-09 01:02:33 +0300

    chore: add garage page reducer to store

--- async-race/src/model/store.ts
+++ async-race/src/model/store.ts
@@ -4,13 +4,15 @@ import pageReducer from './feature/pages/slice';
 import garageReducer from './feature/garage/slice';
 import createCarRedcuer from './feature/createCar';
 import updateCarReducer from './feature/updateCar';
+import garagePageReducer from './feature/garagePages';
 
 const store = configureStore({
   reducer: combineReducers({
     pages: pageReducer,
     garage: garageReducer,
     createCar: createCarRedcuer,
-    updateCar: updateCarReducer
+    updateCar: updateCarReducer,
+    garagePage: garagePageReducer,
   })
 });
 
